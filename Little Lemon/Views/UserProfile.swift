83b107f45cdc36140b44f5d45129374a8f7c206d
Bruce <bruce@redravencomputing.com>
2023-10-27 00:29:09 -0500

    Update the UserProfile section

--- Little Lemon/Views/UserProfile.swift
+++ Little Lemon/Views/UserProfile.swift
@@ -14,36 +14,79 @@ struct UserProfile: View {
     @Binding var profileImage : Image
     
     // Create constants to hold user information from UserDefaults
-    let firstName: String = UserDefaults.standard.string(forKey: kFirstName) ?? ""
-    let lastName: String = UserDefaults.standard.string(forKey: kLastName) ?? ""
-    let email: String = UserDefaults.standard.string(forKey: kEmail) ?? ""
+    @State var firstName: String = UserDefaults.standard.string(forKey: kFirstName) ?? ""
+    @State var lastName: String = UserDefaults.standard.string(forKey: kLastName) ?? ""
+    @State var email: String = UserDefaults.standard.string(forKey: kEmail) ?? ""
     
     
     var body: some View {
-        VStack(spacing:20) {
-            Text("Personal Information")
-                .padding()
-                .fontWeight(.semibold)
-                .font(.largeTitle)
+        VStack(alignment: .leading) {
+            VStack {
+                Text("Personal Information")
+                    .padding()
+                    .fontWeight(.semibold)
+                    .font(.largeTitle)
+                    .foregroundStyle(Color.primary2)
+                
+                HStack {
+                    VStack {
+                        Text("Avatar")
+                            .font(.caption)
+                            .foregroundStyle(Color.secondary3)
+                        profileImage
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .padding(.bottom, 20)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Button("Change Avatar") {
+                        //Show camera or photo album and replace and save image
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 10)
+                    .background(Color.primary2)
+                    .tint(Color.secondary4)
+                    .cornerRadius(8)
+                    
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .background(Color.primary1)
             
-            profileImage
-                .resizable()
-                .frame(width: 100, height: 100)
-                .clipShape(Circle())
-                .padding(.bottom, 20)
+            VStack(alignment: .leading, spacing: 8){
+                Text("First Name")
+                TextField("First Name", text: $firstName)
+                      .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal, 20)
             
-            Text("First Name: \(firstName)")
-                .font(.title3)
-            Text("Last Name: \(lastName)")
-                .font(.title3)
-            Text("Email: \(email)")
-                .font(.title3)
+            VStack(alignment: .leading, spacing: 8){
+                Text("Last Name")
+                TextField("First Name", text: $lastName)
+                      .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal, 20)
             
-            Spacer()
-            
-            Button("Logout") {
-                UserDefaults.standard.set(false, forKey: kIsLoggedIn)
-                appState.isLoggedIn = false
+            VStack(alignment: .leading, spacing: 8){
+                Text("Email")
+                TextField("First Name", text: $email)
+                      .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal, 20)
+        }
+        
+       
+        
+        VStack{
+            Button("Save Changes") {
+                UserDefaults.standard.set(firstName, forKey: kFirstName)
+                UserDefaults.standard.set(lastName, forKey: kLastName)
+                UserDefaults.standard.set(email, forKey: kEmail)
             }
             .padding(.vertical, 10)
             .padding(.horizontal, 100)
@@ -52,7 +95,21 @@ struct UserProfile: View {
             .cornerRadius(8)
             
             Spacer()
+            
+            Button("Logout") {
+                UserDefaults.standard.set(false, forKey: kIsLoggedIn)
+                appState.isLoggedIn = false
+            }
+            .padding(.vertical, 10)
+            .padding(.horizontal, 150)
+            .background(Color.primary2)
+            .tint(Color.secondary4)
+            .cornerRadius(8)
+            .fontWeight(.bold)
+            
         }
+        .padding()
+
     }
 }
 
